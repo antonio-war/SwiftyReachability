a8d4106d849c81c81ef8f695823b579b888b9d89
AntonioWar <antonioguerra.it@gmail.com>
2022-09-02 10:21:49 +0200

    refactor: remove test target

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "ConnectionManager",
             dependencies: []),
-        .testTarget(
-            name: "ConnectionManagerTests",
-            dependencies: ["ConnectionManager"]),
     ]
 )
