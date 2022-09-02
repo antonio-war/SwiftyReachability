2cd9960437c008cee0032bd9c05d176a0898455d
AntonioWar <antonioguerra.it@gmail.com>
2022-09-02 12:51:44 +0200

    feat: create base skeleton

--- Sources/ConnectionManager/ConnectionManager.swift
+++ Sources/ConnectionManager/ConnectionManager.swift
@@ -1,6 +1,63 @@
-public struct ConnectionManager {
-    public private(set) var text = "Hello, World!"
+//
+//  ConnectionManager.swift
+//
+//
+//  Created by AntonioWar on 02/09/22.
+//
 
-    public init() {
+import Network
+
+public class ConnectionManager {
+    
+    public static var shared : ConnectionManager = ConnectionManager()
+    
+    private let monitor : NWPathMonitor
+    
+    private var semaphore : DispatchSemaphore? = DispatchSemaphore(value: 0)
+    
+    public var connectionType: ConnectionType? {
+        didSet {
+            semaphore?.signal()
+        }
+    }
+    
+    public var isConnected: Bool {
+        return connectionType != nil
+    }
+    
+    private init() {
+        monitor = NWPathMonitor.init()
+                    
+        monitor.pathUpdateHandler = { path in
+            self.setConnection(path: path)
+        }
+        
+        monitor.start(queue: DispatchQueue.global(qos: .background))
+        
+        semaphore?.wait()
+        semaphore = nil
+    }
+    
+    private func setConnection(path: NWPath) {
+        guard path.status == .satisfied else {
+            if self.connectionType != .none {
+                self.connectionType = .none
+            }
+            return
+        }
+        
+        if path.usesInterfaceType(.cellular) {
+            if self.connectionType != .cellular {
+                self.connectionType = .cellular
+            }
+        } else if path.usesInterfaceType(.wifi) {
+            if self.connectionType != .wifi {
+                self.connectionType = .wifi
+            }
+        } else if path.usesInterfaceType(.wiredEthernet) {
+            if self.connectionType != .ethernet {
+                self.connectionType = .ethernet
+            }
+        }
     }
 }
---
+++ Sources/ConnectionManager/ConnectionType.swift
@@ -0,0 +1,14 @@
+//
+//  ConnectionType.swift
+//  
+//
+//  Created by AntonioWar on 02/09/22.
+//
+
+import Foundation
+
+public enum ConnectionType {
+    case cellular
+    case wifi
+    case ethernet
+}
